01f9948806c292e9e659a7be1e09ca13c937bb99
jangresearchlab <janglab321@gmail.com>
2026-01-12 22:39:57 +0000

    Deploying to gh-pages from @ jangresearchlab/jangresearchlab.github.io@cf470996494e0b6c9615bd758d4c56f5d3ee7e0f 🚀

--- assets/js/giscus-setup.js
+++ assets/js/giscus-setup.js
@@ -18,7 +18,7 @@ function determineGiscusTheme() {
 
   let giscusAttributes = {
     src: "https://giscus.app/client.js",
-    "data-repo": "JangLab321/janglab.github.io",
+    "data-repo": "jangresearchlab/jangresearchlab.github.io",
     "data-repo-id": "",
     "data-category": "Comments",
     "data-category-id": "",
